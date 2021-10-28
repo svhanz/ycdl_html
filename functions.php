de510b573327524419b78fdc50be1ab2e68a5522
svhanz <serg-69@mail.ru>
2021-10-28 22:19:52 +0300

    master

--- functions.php
+++ functions.php
@@ -2,10 +2,12 @@
 add_action( 'wp_enqueue_scripts', 'ycdl_scripts' );
 function ycdl_scripts() {
 // connect styles
-wp_enqueue_style( 'main', get_stylesheet_uri() );
+wp_enqueue_style( 'style', get_stylesheet_uri() );
 wp_enqueue_style( 'ycdl', get_template_directory_uri() . '/css/swiper-bundle.min.css', array('main'), null );
-wp_enqueue_style( 'ycdl', get_template_directory_uri() . '/css/style.css', array('main'), null );
+wp_enqueue_style( 'ycdl', get_template_directory_uri() . '/css/main.css', array('main'), null );
 
 
-wp_enqueue_script( 'script-name', get_template_directory_uri() . '/js/example.js', array(), '1.0.0', true );
+wp_enqueue_script( 'swiper', get_template_directory_uri() . '/js/swiper-bundle.min.js', array(), '1.0.0', true );
+wp_enqueue_script( 'script', get_template_directory_uri() . '/js/script.js', array(), '1.0.0', true );
+
 }
